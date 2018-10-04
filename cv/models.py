770cfdd5596cccc592f1bfbf944e8cc8e8d3b308
Robrens <jbb.baudu@wanadoo.fr>
2018-10-04 12:02:30 +0200

    translate in english and some modification of models

--- cv/models.py
+++ cv/models.py
@@ -19,11 +19,11 @@ class Category(models.Model):
 class Experience(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    address = models.TextField(max_length=100)
+    address = models.TextField(null=True, blank=True, max_length=100)
     short_desc = models.TextField(max_length=100)
     content = models.TextField(max_length=3000)
-    start_the = models.DateField(auto_now_add=False, verbose_name="Date de debut")
-    end_the = models.DateField(auto_now=False, verbose_name="Date de fin")
+    start_the = models.DateField(null=True, blank=True, auto_now_add=False, verbose_name="Date de debut")
+    end_the = models.DateField(null=True, blank=True, auto_now=False, verbose_name="Date de fin")
     cover = models.ImageField(upload_to="articles")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
 
