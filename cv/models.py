287580a958aec5c6f02f74596feb16c098798e5b
Robrens <jbb.baudu@wanadoo.fr>
2018-10-30 13:04:26 +0100

    sparetime css modification

--- cv/models.py
+++ cv/models.py
@@ -48,10 +48,10 @@ class Experience(models.Model):
     address = models.TextField(null=True, blank=True, max_length=100)
     short_desc = models.TextField(null=True, blank=True, max_length=100)
     content = models.TextField(null=True, blank=True, max_length=3000)
-    start_the = models.DateField(
-        null=True, blank=True, auto_now_add=False, verbose_name="Date de debut")
-    end_the = models.DateField(
-        null=True, blank=True, auto_now=False, verbose_name="Date de fin")
+    start_the = models.CharField(
+        null=True, blank=True, max_length=100, verbose_name="Date de debut")
+    end_the = models.CharField(
+        null=True, blank=True, max_length=100, verbose_name="Date de fin")
     cover = models.ImageField(null=True, blank=True, upload_to="articles")
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, null=False)
